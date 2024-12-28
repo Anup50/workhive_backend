7949070d7f6f01700bf9c43754c0135fa283617c
Anup50 <kca3734@gmail.com>
2024-12-28 13:21:48 +0545

    role schema

---
+++ model/Role.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const roleSchema = new mongoose.Schema({
+  role_name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+const Role = mongoose.model("Role", roleSchema);
+
+module.exports = Role;
