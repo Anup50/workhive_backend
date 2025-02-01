239d14701208b527c5a3ef2b7f799acc59205051
Anup50 <kca3734@gmail.com>
2025-02-01 19:20:34 +0545

    created job  model

---
+++ model/Job.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const JobSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  employer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Employer", // Reference to the Employer collection
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  salary: {
+    type: Number,
+    required: true,
+  },
+  jobType: {
+    type: String,
+    enum: ["Full-time", "Part-time", "Contract", "Freelance"],
+    required: true,
+  },
+  experienceLevel: {
+    type: String,
+    enum: ["Entry", "Mid", "Senior"],
+    required: true,
+  },
+  datePosted: {
+    type: Date,
+    default: Date.now,
+  },
+  deadline: {
+    type: Date,
+  },
+  skillsRequired: {
+    type: [String],
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const Job = mongoose.model("Job", JobSchema);
+
+module.exports = Job;
