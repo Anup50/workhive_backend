f537f002cc79244270dca940bc7fafb1bd9b23d8
Anup50 <kca3734@gmail.com>
2025-02-02 18:54:02 +0545

    employer route

---
+++ routes/EmployerRoute.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const { add, findAll, update } = require("../controller/EmployerController");
+const { authenticateToken, authorize } = require("../security/Auth");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "companylogo_images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+router.get("/", findAll);
+router.post("/add", upload.single("companyLogo"), add);
+router.put("/update/:id", upload.single("companyLogo"), update);
+
+module.exports = router;
