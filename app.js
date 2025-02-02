d06e9790768c83192bb5be72b1302916396580a5
Anup50 <kca3734@gmail.com>
2025-02-02 17:28:11 +0545

    created route handlers and set enabled CORS

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const RoleRouter = require("./routes/RoleRoute");
+const UserRouter = require("./routes/UserRoute");
+const EmployerRouter = require("./routes/EmployerRoute");
+const JobRouter = require("./routes/JobRouter");
+const JobSeekerRouter = require("./routes/JobSeekerRoute");
+const ResumeRouter = require("./routes/ResumeRoute");
+const ApplicationRouter = require("./routes/ApplicationRoute");
+const BookmarkRouter = require("./routes/BookmarkRoute");
+const app = express();
+
+connectDB();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+app.use("/api/role", RoleRouter);
+app.use("/api/user", UserRouter);
+app.use("/api/employer", EmployerRouter);
+app.use("/api/job", JobRouter);
+app.use("/api/jobseeker", JobSeekerRouter);
+app.use("/api/resume", ResumeRouter);
+app.use("/api/application", ApplicationRouter);
+app.use("/api/bookmark", BookmarkRouter);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Running at http://localhost:${port}`);
+});
