63ba95aac91fb052043192c8a4293feac7e322dd
Anup50 <kca3734@gmail.com>
2025-02-06 23:19:39 +0545

    removed comments

--- service/JobSeekerService.js
+++ service/JobSeekerService.js
@@ -1,52 +1,3 @@
-// const fs = require("fs");
-// const path = require("path");
-// const JobSeeker = require("../model/JobSeeker");
-
-// class JobSeekerService {
-//   async createJobSeeker(jobSeekerData) {
-//     try {
-//       const newJobSeeker = await JobSeeker.create(jobSeekerData);
-//       return newJobSeeker;
-//     } catch (error) {
-//       throw new Error(`Error creating job seeker: ${error.message}`);
-//     }
-//   }
-
-//   async getJobSeekerById(id) {
-//     return await JobSeeker.findById(id).populate("userId");
-//   }
-
-//   async updateJobSeeker(id, updateData) {
-//     try {
-//       const existingJobSeeker = await JobSeeker.findById(id);
-
-//       if (existingJobSeeker.profilePicture && updateData.profilePicture) {
-//         const oldImagePath = path.join(
-//           __dirname,
-//           "..",
-//           "uploads",
-//           existingJobSeeker.profilePicture
-//         );
-//         fs.unlinkSync(oldImagePath);
-//       }
-
-//       // Update the job seeker with new data
-//       return await JobSeeker.findByIdAndUpdate(id, updateData, { new: true });
-//     } catch (error) {
-//       throw new Error(`Error updating job seeker: ${error.message}`);
-//     }
-//   }
-
-//   async deleteJobSeeker(id) {
-//     return await JobSeeker.findByIdAndDelete(id);
-//   }
-
-//   async getAllJobSeekers(filter = {}) {
-//     return await JobSeeker.find(filter).populate("userId");
-//   }
-// }
-
-// module.exports = new JobSeekerService();
 const fs = require("fs");
 const path = require("path");
 const JobSeeker = require("../model/JobSeeker");
@@ -67,7 +18,6 @@ class JobSeekerService {
 
   async updateJobSeeker(id, updateData) {
     try {
-      // Fetch the existing JobSeeker
       const existingJobSeeker = await JobSeeker.findById(id);
 
       if (!existingJobSeeker) {
@@ -81,7 +31,6 @@ class JobSeekerService {
           existingJobSeeker.profilePicture
         );
 
-        // Check if the file exists before attempting to delete
         if (fs.existsSync(oldImagePath)) {
           fs.unlinkSync(oldImagePath); // Synchronous deletion
           console.log(`Deleted old profile picture: ${oldImagePath}`);
@@ -90,7 +39,6 @@ class JobSeekerService {
         }
       }
 
-      // Update the JobSeeker with the new data
       const updatedJobSeeker = await JobSeeker.findByIdAndUpdate(
         id,
         updateData,
