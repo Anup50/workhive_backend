00158a2636eacdf7993ca5e93c3d1357469069cc
Anup50 <kca3734@gmail.com>
2025-02-18 18:51:32 +0545

    modified authenticate token to check for token first then http cookie for jobseerkerid extrction

--- security/Auth.js
+++ security/Auth.js
@@ -3,10 +3,19 @@ const SECRET_KEY =
   "fef3b170bd182cb817e569f32af534fa372e5eab3b537a5e2b5a06f3a9522c260afa3aa440463b3c32d09177edd60dc8d9f9e8fb56ca4a6684f24e5a44835360";
 
 function authenticateToken(req, res, next) {
-  const token = req.header("Authorization")?.split(" ")[1];
+  console.log("Authorization header:", req.header("Authorization"));
+  console.log("Cookies:", req.cookies);
+
+  let token = req.header("Authorization")?.split(" ")[1];
+
+  if (!token && req.cookies && req.cookies.token) {
+    token = req.cookies.token;
+  }
+
   if (!token) {
     return res.status(401).send("Access denied: No token provided");
   }
+
   try {
     const verified = jwt.verify(token, SECRET_KEY);
     req.user = verified;
@@ -19,11 +28,12 @@ function authenticateToken(req, res, next) {
 function authorize(role) {
   return (req, res, next) => {
     if (req.user.role !== role) {
-      return res.status(403).send("Access Denied:Insufficient Permissions");
+      return res.status(403).send("Access Denied: Insufficient Permissions");
     }
     next();
   };
 }
+
 function allowSelfOrRole(role) {
   return (req, res, next) => {
     if (req.params.id === req.user.id || req.user.role === role) {
