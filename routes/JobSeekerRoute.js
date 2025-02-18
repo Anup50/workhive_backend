ebb1ce6d07b30cbf4d236603380849bc079b10ed
Anup50 <kca3734@gmail.com>
2025-02-18 18:58:21 +0545

    added router for getjobseekerid

--- routes/JobSeekerRoute.js
+++ routes/JobSeekerRoute.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const jobSeekerController = require("../controller/JobSeekerController.js");
 const upload = require("../middleware/ProfilePicMiddleware.js");
-
+const { authenticateToken } = require("../security/Auth");
 const router = express.Router();
-
+router.get(
+  "/getJobSeekerId",
+  authenticateToken,
+  jobSeekerController.getJobSeekerId
+);
 router.post("/", jobSeekerController.create);
 router.post("/uploadImage", upload, jobSeekerController.uploadImage);
 router.get("/:id", jobSeekerController.getById);
 router.put("/update/:id", upload, jobSeekerController.update);
-
 router.delete("/:id", jobSeekerController.delete);
 
 module.exports = router;
